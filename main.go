2b183b25ad801ff4fadb3a31e6e82d7d6255a092
Samuel M. <samuel@meatball.ch>
2025-09-22 11:29:42 +0200

    P2-2: Now already included basic enpoint structure for API

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"biketouren/endpoints"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/bike", endpoints.Bike)
+	router.GET("/tour", endpoints.Tour)
 	router.Run() // listens on 0.0.0.0:8080 by default
 }
